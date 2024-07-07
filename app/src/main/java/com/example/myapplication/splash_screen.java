11dc4f4df7660b855846424fcfac0e638611b814
Inbsaatzainab22 <Izainabalvi@gmail.com>
2024-07-07 21:57:40 +0500

    Final commit of semester

--- app/src/main/java/com/example/myapplication/splash_screen.java
+++ app/src/main/java/com/example/myapplication/splash_screen.java
@@ -27,13 +27,13 @@ public class splash_screen extends AppCompatActivity {
         binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        //  setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_splash_screen);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+      /*  binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -48,5 +48,8 @@ public class splash_screen extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_splash_screen);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+       */
     }
 }
